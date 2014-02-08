a34a67a92c0a195e6db5aa7692d04e7b93b2e1e0
QingDengKuZhu <2583746755@qq.com>
2014-02-08 20:07:14 +0800

    快速找到未知长度单链表的中间结点

--- LinkList/Data_Expand.c
+++ LinkList/Data_Expand.c
@@ -2,3 +2,44 @@
 #include "Main_First.h"
 #include "Data_Base.h"
 #include "Data_Expand.h"
+
+PNODE FindMidNode(PLINKLIST pL)
+{
+	/*法一
+	**首先遍历一遍单链表以确定单链表的长度L.
+	**然后再次从头结点出发循环L/2次找到单链
+	**表的中间结点.
+	**算法复杂度为:O(L+L/2) = O(3L/2).
+	
+	PNODE p = pL->pNext;
+	size_t i = 1;
+	size_t length = ListLength(pL);
+
+	while (p && i<(leng+1)/2)
+	{
+		p = p->pNext;
+		++i;
+	}
+
+	return p;
+	*/
+	
+	/*法二(推荐)
+	**利用快慢指针
+	*/
+	PNODE mid = pL;
+	PNODE p = pL;
+
+	while(p && p->pNext) /*p和p->pNext的顺序绝对不能调换!!!*/
+	{
+		mid = mid->pNext;
+		p = p->pNext->pNext;
+	}
+
+	if (p == pL)
+	{
+		return NULL;
+	}
+
+	return mid;
+}
--- LinkList/Data_Expand.h
+++ LinkList/Data_Expand.h
@@ -9,4 +9,7 @@
 #include "Data_Base.h"
 
 
+/*快速找到未知长度单链表的中间结点.若找到该结点,返回地址;否则,返回NULL.*/
+PNODE FindMidNode(PLINKLIST pL);
+
 #endif
